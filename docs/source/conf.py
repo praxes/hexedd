ff007b51e9a43d144e64c0186cd6fa65e06a2350
Darren Dale <dsdale24@gmail.com>
2015-12-15 13:28:06 -0500

    adds github sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import hexedd
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../sphinxext'))
 
 # -- General configuration ------------------------------------------------
 
@@ -40,6 +40,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'github',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
